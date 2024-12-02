813f433436f09211a484628b4b773ac6debe33d6
yanioconjota <janioisacura@gmail.com>
2024-12-02 14:32:39 -0300

    Created bolierplate

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* The line `app.setGlobalPrefix('api');`
+  in the provided TypeScript code is setting a global prefix
+  for all routes in the NestJS application. In this case,
+  the global prefix is set to 'api', which means that
+  all routes defined in the application will be prefixed with '/api'. */
+
+  app.setGlobalPrefix('api/v2');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
