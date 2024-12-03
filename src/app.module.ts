d95bd06c7f039b33e7d29030679a937c9b5e5871
yanioconjota <janioisacura@gmail.com>
2024-12-03 18:38:59 -0300

    Added Seed Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SharedModule } from './shared/shared.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { SharedModule } from './shared/shared.module';
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
-    SharedModule
+    SharedModule,
+    SeedModule
   ],
   controllers: [],
   providers: [],
